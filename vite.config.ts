9bcb49106147dfe4e93263afe0a60edaf2bba245
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-12-17 20:13:53 -0800

    Enable root import with @

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import basicSsl from "@vitejs/plugin-basic-ssl"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), basicSsl()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+			"#root": resolve(__dirname),
+		},
+	},
 })
